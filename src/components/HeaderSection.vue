5a3cabc39427b8dc04afe5241c5f2881b93e6b55
hyunminkim93 <rlagusals234@gmail.com>
2024-06-11 16:11:10 +0900

    port1

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -19,3 +19,63 @@
     </div>
   </header>
 </template>
+
+<style lang="scss">
+@import '@/assets/scss/mixin';
+#header {
+  @include position-fixed;
+  z-index: 10000;
+
+  .header__inner {
+    @include flex-between;
+    background-color: rgba(116, 99, 99, 0.1);
+    backdrop-filter: blur(15px);
+    padding: 1rem;
+
+    .header__logo {
+      font-size: 2rem;
+      text-align: center;
+      text-transform: uppercase;
+      line-height: 1;
+
+      em {
+        font-size: 13px;
+        display: block;
+        color: var(--black200);
+      }
+    }
+    .header__nav {
+      li {
+        display: inline;
+
+        a {
+          text-transform: uppercase;
+          font-size: 1rem;
+          padding: 14px;
+          position: relative;
+          font-weight: 400;
+
+          &::before {
+            content: '';
+            height: 1px;
+            width: calc(100% - 28px);
+            background-color: var(--black);
+            position: absolute;
+            left: 14px;
+            bottom: 10px;
+            transform: scaleX(0);
+            transition: all 0.3s;
+          }
+
+          &:hover::before {
+            transform: scaleX(1);
+          }
+        }
+      }
+    }
+    .header__nav__mobile {
+      display: none;
+    }
+  }
+}
+</style>
