22e73e18e09379eef4fb1be75fdf2ee48c67039a
hyunminkim93 <rlagusals234@gmail.com>
2024-06-12 16:51:50 +0900

    최종완성

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,205 @@ export const headerNav = [
     url: '#contact'
   }
 ]
+
+export const skillText = [
+  {
+    title: '신입 개발자의 꿈: 무한한 가능성의 시작',
+    description:
+      '신입 개발자에게 있어 꿈은 단순한 목표가 아니라 무한한 가능성의 시작입니다. 기술의 세계는 빠르게 변화하며, 매일 새로운 도전과 기회가 기다리고 있습니다. 처음에는 작은 코드 한 줄이었을지라도, 이 작은 코드가 모여 혁신적인 소프트웨어를 만들고, 세상을 변화시킬 수 있는 도구가 됩니다. 꿈을 가지고 있는 신입 개발자는 이러한 가능성을 두려워하지 않고, 끊임없이 배우고 성장하며, 자신의 한계를 뛰어넘을 준비가 되어 있습니다. 그들의 꿈은 곧 미래의 기술 혁신을 이끄는 원동력이 될 것입니다.'
+  },
+  {
+    title: '신입 개발자의 의지: 끈기와 도전의 연속',
+    description:
+      '신입 개발자의 여정은 끊임없는 도전과 역경의 연속입니다. 새로운 기술을 배우고, 복잡한 문제를 해결하며, 종종 예상치 못한 오류와 마주하게 됩니다. 이러한 과정에서 중요한 것은 바로 포기하지 않는 의지입니다. 끈기와 도전 정신을 가지고 끊임없이 노력하는 신입 개발자는 결국 자신의 능력을 입증하고, 더 나은 개발자로 성장할 수 있습니다. 이들의 의지는 단순히 개인의 성공을 넘어, 팀과 조직의 성공을 함께 이끌어가는 중요한 원동력이 됩니다.'
+  },
+  {
+    title: '신입 개발자의 열정: 혁신과 창의성의 원천',
+    description:
+      '열정은 신입 개발자가 가진 가장 큰 무기 중 하나입니다. 새로운 기술에 대한 호기심, 더 나은 코드를 작성하고자 하는 열망, 그리고 사용자에게 최고의 경험을 제공하고자 하는 마음가짐은 이들을 더욱 창의적이고 혁신적으로 만듭니다. 열정적인 신입 개발자는 기존의 틀을 깨고, 새로운 아이디어를 실현시키며, 기술의 경계를 넓혀 나갑니다. 이들의 열정은 조직의 문화와 분위기를 긍정적으로 변화시키고, 더 나은 세상을 만들어가는 데 큰 기여를 합니다.'
+  }
+]
+
+export const siteText = [
+  {
+    text: ['make', 'site compliant with', 'webstandard'],
+    title: '비트를 이용한 사이트 제작',
+    code: 'https://github.com/webstoryboy/port2023-vite',
+    view: 'https://port2023-vite.netlify.app',
+    info: ['site coding', 'production period : two days', 'use stack : HTML5/CSS3, CSS Variable, Vite']
+  },
+  {
+    text: ['make', 'site compliant with', 'react.js'],
+    title: '리액트를 이용한 사이트 제작',
+    code: 'https://github.com/webstoryboy/port2023-react',
+    view: 'https://port2023-react.netlify.app',
+    info: ['site coding', 'production period : two days', 'use stack : HTML5/CSS3, CSS Variable, Vite']
+  },
+  {
+    text: ['make', 'site compliant with', 'vue.js'],
+    title: '뷰를 이용한 사이트 제작',
+    code: 'https://github.com/webstoryboy/port2023-vue',
+    view: 'https://port2023-vue.netlify.app',
+    info: ['site coding', 'production period : two days', 'use stack : HTML5/CSS3, Scss Variable, vue']
+  },
+  {
+    text: ['make', 'site compliant with', 'next.js'],
+    title: '넥스트를 이용한 사이트 제작',
+    code: 'https://github.com/webstoryboy/port2023-next',
+    view: 'https://port2023-next.netlify.app',
+    info: ['site coding', 'production period : two days', 'use stack : HTML5/CSS3, Scss Variable, next.js']
+  }
+]
+
+import port01 from '../assets/img/port01.jpg'
+import port02 from '../assets/img/port02.jpg'
+import port03 from '../assets/img/port03.jpg'
+import port04 from '../assets/img/port04.jpg'
+import port05 from '../assets/img/port05.jpg'
+import port06 from '../assets/img/port06.jpg'
+import port07 from '../assets/img/port07.jpg'
+import port08 from '../assets/img/port08.jpg'
+import port09 from '../assets/img/port09.jpg'
+import port10 from '../assets/img/port10.jpg'
+
+export const portText = [
+  {
+    num: '01',
+    title: '어워드에도 올라간 포트폴리오',
+    desc: '라마 디자인을 통해 자신의 스킬을 가장 멋지게 표현한 포트폴리오입니다. 가로 모드와 세로 모드는 매우 인상적이며 특히 리액트와 비트를 이용하여 제작한 것은 더욱 놀랍습니다. 이 사이트는 awwwards.com에도 인정받아 올라간 포트폴리오입니다. 확실히 그의 뛰어난 디자인 스킬과 기술력이 빛을 발휘한 결과물인 것 같습니다.',
+    img: port01,
+    code: 'https://github.com/kimsangjunv1/-React-Portfolio',
+    view: 'https://portfoliosj-react.netlify.app',
+    name: '김상* 포트폴리오'
+  },
+  {
+    num: '02',
+    title: '빛나는 밤에 포트폴리오',
+    desc: '이 사이트는 정말 인상적인 포트폴리오입니다. 특히 스무스한 효과와 가로 모드드 높은 퀄리티를 자랑합니다. 디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼 수 있습니다. GSAP와 React.js를 이용하여 사이트를 표현한 것은 기술적인 능력과 창의성을 강조하는데, 부족함이 없으며, 세심한 코딩과 디테일한 작업으로 그의 개발 감각과 능력이 빛을 발휘한 것 같습니다. ',
+    img: port02,
+    code: 'https://github.com/seolhee313/PORTFOLIO-REACT',
+    view: 'https://portfolio-313.web.app/',
+    name: '천설* 포트폴리오'
+  },
+  {
+    num: '03',
+    title: '열정이 넘치는 포트폴리오',
+    desc: '이 사이트는 정말 인상적인 포트폴리오입니다. 특히 스무스한 효과와 가로 모드드 높은 퀄리티를 자랑합니다. 디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼 수 있습니다. GSAP와 React.js를 이용하여 사이트를 표현한 것은 기술적인 능력과 창의성을 강조하는데, 부족함이 없으며, 세심한 코딩과 디테일한 작업으로 그의 개발 감각과 능력이 빛을 발휘한 것 같습니다. ',
+    img: port03,
+    code: 'https://github.com/seolhee313/PORTFOLIO-REACT',
+    view: 'https://portfolio-313.web.app/',
+    name: '천설* 포트폴리오'
+  },
+  {
+    num: '04',
+    title: '모던한 포트폴리오',
+    desc: '블랙 컨셉과 애니메이션이 돋보이는 포트폴리오 사이트입니다. GSAP를 통한 애니메이션과 NEXT.js를 통해 제작된 포트폴리오입니다. pin 애니메이션을 통한 포폴 작업물의 표현 능력이 돋보이는 사이트입니다.',
+    img: port04,
+    code: 'https://github.com/dlgnsrb227/portfolio-next',
+    view: 'https://hoongportfolio-next.netlify.app/',
+    name: '이훈* 포트폴리오'
+  },
+  {
+    num: '05',
+    title: '가로모드의 정석 포트폴리오',
+    desc: '이 포트폴리오는 가로모드를 통해 눈에 띄는 애니메이션 효과를 가진 멋진 작품들이 펼쳐집니다. 세션 간의 부드러운 전환과 흥미로운 움직임이 사용자들에게 색다른 경험을 선사합니다. 사이트에는 탁월한 디자인과 창의적인 애니메이션들이 어우러져, 사용자들에게 인상적인 시각적인 효과를 줍니다. 애니메이션은 적절히 사용되어 사이트를 더욱 생동감 있게 만들어주는 포트폴리오입니다.',
+    img: port05,
+    code: '/',
+    view: 'https://junseungpark.github.io/portfolio/index3.html',
+    name: '박준* 포트폴리오'
+  },
+  {
+    num: '06',
+    title: '화려함의 정석 포트폴리오',
+    desc: '화려하고 세련된 디자인과 다채로운 색상이 사용된 포트폴리오는 사용자를 홀릴 듯한 시각적인 매력을 지니고 있습니다. 포트폴리오 내의 각 작품들은 디테일한 디자인과 탁월한 시각적 표현력을 갖추고 있어, 주인공의 뛰어난 예술적 감각을 느낄 수 있습니다. 화려한 애니메이션 효과와 부드러운 전환은 사이트를 더욱 생동감 있게 만들어주며, 사용자들에게 색다른 경험을 선사합니다.',
+    img: port06,
+    code: '/',
+    view: 'https://webstoryboy.github.io/port2023/portfolio-student/DavidYang/index.html',
+    name: '포트폴리오'
+  },
+  {
+    num: '07',
+    title: '패럴랙스 정석 포트폴리오',
+    desc: '마치 예술작품을 감상하는 듯한 환상적인 경험을 선사하는 포트폴리오입니다. 패럴랙스 스크롤링을 활용하여 구성된 사이트는 사용자들에게 독특하고 멋진 시각적 효과를 제공합니다. 배경과 움직이는 요소들이 조화롭게 어우러져, 사이트 전반에 걸쳐 깊이와 입체감을 느낄 수 있습니다. 스크롤에 따라 움직이는 요소들은 마치 세계를 탐험하는 듯한 느낌을 주며, 사용자들을 끌어들이는 매력적인 요소로 작용합니다.',
+    img: port07,
+    code: '/',
+    view: 'https://webstoryboy.github.io/port2023/portfolio-student/TaeyongLee/index.html',
+    name: '포트폴리오'
+  },
+  {
+    num: '08',
+    title: '트랜지션 포트폴리오',
+    desc: '화면 전환과 요소들의 흐름이 순조롭고 매끄러운 작품들로 가득한 포트폴리오 사이트입니다. 페이지 간의 트랜지션은 마치 이야기를 풀어내는 듯한 흥미진진한 경험을 선사합니다. 트랜지션 효과의 적절한 활용은 작품들을 보다 동적이고 생동감 있게 만들어줍니다. 각 페이지의 이동이 자연스럽고 사용자들이 원활하게 사이트를 탐색할 수 있도록 배려된 구성은 개발자의 디자인 능력을 잘 보여주는 특징입니다.',
+    img: port08,
+    code: '/',
+    view: 'https://webstoryboy.github.io/port2023/portfolio-student/HyunroKim/index.html',
+    name: '포트폴리오'
+  },
+  {
+    num: '09',
+    title: '스크롤의 정석 포트폴리오',
+    desc: '스크롤링을 활용하여 훌륭한 사용자 경험을 선사하는 포트폴리오 사이트입니다. 스크롤을 내리면서 작품들이 순차적으로 나타나고 효과적으로 전환되는 것은 마치 예술적인 이야기를 읽어나가는 듯한 느낌을 주며 사용자를 매료시킵니다. 스크롤의 움직임을 통해 작품들이 서서히 드러나고, 각 페이지 간의 전환은 자연스럽고 부드럽습니다. 이러한 트랜지션과 애니메이션들이 작품들의 내용과 테마를 더욱 강조해줍니다.',
+    img: port09,
+    code: '/',
+    view: 'https://webstoryboy.github.io/port2023/portfolio-student/ChoJaeHyung/index.html',
+    name: '포트폴리오'
+  },
+  {
+    num: '10',
+    title: '모던함의 정석 포트폴리오',
+    desc: '현대적이고 세련된 디자인으로 가득한 포트폴리오 사이트입니다. 모던한 느낌과 세심한 디테일이 조화를 이루며, 사용자에게 신선하고 품격 있는 경험을 제공합니다. 사이트의 디자인은 깔끔하고 단정하면서도 특유의 감성과 풍부한 표현력이 느껴집니다. 간결한 레이아웃과 모던한 색상 선택은 주인공의 디자인 감각을 잘 보여주는 특징입니다.',
+    img: port10,
+    code: '/',
+    view: 'https://webstoryboy.github.io/port2023/portfolio-student/JooHyeji/index.html',
+    name: '포트폴리오'
+  }
+]
+
+export const contactText = [
+  {
+    link: 'https://www.instagram.com/hyunminzz/',
+    title: 'Insta : hyunminzz'
+  },
+  {
+    link: 'mailto:rlagusals234@gmail.com',
+    title: 'Mail : rlagusals234@gmail.com'
+  }
+]
+
+export const footerText = [
+  {
+    title: 'youtube',
+    desc: '유튜브에 오시면 더 많은 강의를 볼 수 있습니다.',
+    link: 'https://www.youtube.com/@Webstoryboy'
+  },
+  {
+    title: 'github',
+    desc: '깃헙에 오시면 더 많은 소스를 볼 수 있습니다.',
+    link: 'https://github.com/hyunminkim93'
+  },
+  {
+    title: 'blog',
+    desc: '블로그에 오시면 더 많은 정보를 볼 수 있습니다.',
+    link: 'https://webstoryboy.co.kr'
+  },
+  {
+    title: 'gsap',
+    desc: 'GSAP에 오시면 더 많은 강의를 볼 수 있습니다.',
+    link: 'https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq'
+  },
+  {
+    title: 'react',
+    desc: '리액트로 만든 사이트를 같이 만들어 봅니다.',
+    link: 'https://github.com/webstoryboy/port2023-react'
+  },
+  {
+    title: 'vue',
+    desc: '뷰로 만든 사이트를 같이 만들어 봅니다.',
+    link: 'https://github.com/webstoryboy/port2023-vue'
+  },
+  {
+    title: 'next',
+    desc: '넥스트로 만든 사이트를 같이 만들어 봅니다.',
+    link: 'https://github.com/webstoryboy/port2023-next'
+  }
+]
